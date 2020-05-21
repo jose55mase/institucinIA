f39313010eba6a1f0f772f66ed3bfed6f6bb8d65
jose55mase <josemase55@gmail.com>
2020-05-20 22:30:43 -0500

    actualizacion de home imagens

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,21 @@ import { Component, OnInit, Inject, Input, Output, EventEmitter, ViewChild } fro
   
 })
 export class HomeComponent implements OnInit {
-  ngOnInit(){ }
+  ngOnInit(){
+    var observer= new Observable(observer=>{
+      setInterval(()=>observer.next(Math.round(Math.random()*2)),5000)
+    }) 
+    this.IMAGES[0]   
+    observer.subscribe((data)=>{
+      var indice = parseInt(JSON.stringify(data))
+      this.changeImages = this.IMAGES[indice] 
+    })
+  }
+  
+  public IMAGES = [
+    "./../../../assets/img/images1.jpeg"
+    ,"./../../../assets/img/images2.jpeg"
+    ,"./../../../assets/img/images3.png"
+  ]  
+  public changeImages = this.IMAGES[2];
 }
