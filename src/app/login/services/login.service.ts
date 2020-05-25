a215654ad899139f71e9eca7102a3fadb43c9a4a
jose55mase <josemase55@gmail.com>
2020-05-24 21:01:30 -0500

    actualizacion de home

--- src/app/login/services/login.service.ts
+++ src/app/login/services/login.service.ts
@@ -14,7 +14,7 @@ export class LoginService {
 
   login(): Observable<any> {
     console.log()
-    return this.http.get<any>('http://192.168.1.2:3000/data')
+    return this.http.get<any>('http://192.168.1.3:3000/data')
   }
   
 }
